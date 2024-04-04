e754d805111b75ad853b9e24afb033a870bf2881
BrunoPascoa76 <brunopascoa03@gmail.com>
2024-04-04 09:39:39 +0100

    lab7_1 done

--- lab7/lab7_1/src/test/java/ua/bp/tqs/AppTest.java
+++ lab7/lab7_1/src/test/java/ua/bp/tqs/AppTest.java
@@ -3,18 +3,45 @@ package ua.bp.tqs;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    String baseURI = "https://jsonplaceholder.typicode.com/";
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testTodos200() {
+        given().when().get(baseURI + "todos").then().statusCode(200);
+    }
+
+    @Test
+    public void testTodo4Title() {
+        given().when()
+                .get(baseURI + "todos/" + "4")
+                .then()
+                .statusCode(200)
+                .body("title", equalTo("et porro tempora"));
+    }
+
+    @Test
+    public void testTodos198And199Exist() {
+        given().when()
+                .get(baseURI + "todos")
+                .then()
+                .statusCode(200)
+                .body("id", hasItems(198, 199));
+    }
+
+    @Test
+    public void testTodosResponseTime(){
+        given().when()
+            .get(baseURI+"todos")
+            .then()
+            .statusCode(200)
+            .time(lessThan(2000L));
     }
 }
